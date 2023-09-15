3813da799ac287e5121f7d3e760fca5d401d0ced
l_loizides <lefteris.loizides@mindgeek.com>
2023-09-15 04:00:15 +0300

    add favicons compliant to google guidlines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,13 @@ export default function Home() {
       <Head>
         <title>Lefteris Loizides</title>
         <meta name="description" content="Lefteris Loizides is a software engineer who builds SaaS products and digital experiences for the web" />
-        <link rel="icon" href="/icon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#292929" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://lefterisloizides.com" />
         <meta property="og:title" content="Lefteris Loizides" />
