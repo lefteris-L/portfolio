6f96e2c51d02d1ff802e6af6aa6dd2c8ba991539
l_loizides <lefteris.loizides@mindgeek.com>
2023-09-15 15:30:29 +0300

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,12 @@ export default function Home() {
       <Head>
         <title>Lefteris Loizides</title>
         <meta name="description" content="Lefteris Loizides is a software engineer who builds SaaS products and digital experiences for the web" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#292929" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
         <meta property="og:type" content="website" />
