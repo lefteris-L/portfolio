5f05478d69dae0c94964a56918d92f86014f782f
l_loizides <lefteris.loizides@mindgeek.com>
2023-11-20 02:15:47 +0200

    update classes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,12 +17,12 @@ const Projects = () => (
     <div>
       <ul className="group/list">
         <li className="mb-12">
-          <div className="group project">
-            <div className="projectWrapper"></div>
+          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
             <div className="z-10 sm:order-2 sm:col-span-6">
               <h3>
                 <Link
-                  className="group/link projectLink"
+                  className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
                   href="https://t.me/tmpGmailBot"
                   target="_blank"
                   rel="noreferrer"
@@ -75,12 +75,12 @@ const Projects = () => (
           </div>
         </li>
         <li className="mb-12">
-          <div className="group project">
-            <div className="projectWrapper"></div>
+          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
             <div className="z-10 sm:order-2 sm:col-span-6">
               <h3>
                 <Link
-                  className="group/link projectLink"
+                  className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
                   href="https://logos.lefterisloizides.com"
                   target="_blank"
                   rel="noreferrer"
@@ -136,12 +136,12 @@ const Projects = () => (
           </div>
         </li>
         <li className="mb-12">
-          <div className="group project">
-            <div className="projectWrapper"></div>
+          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
             <div className="z-10 sm:order-2 sm:col-span-6">
               <h3>
                 <Link
-                  className="group/link projectLink"
+                  className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
                   href="https://chat-gpt-api-l2r6.onrender.com/"
                   target="_blank"
                   rel="noreferrer"
@@ -192,12 +192,12 @@ const Projects = () => (
           </div>
         </li>
         <li className="mb-12">
-          <div className="group project">
-            <div className="projectWrapper" />
+          <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
             <div className="z-10 sm:order-2 sm:col-span-6">
               <h3>
                 <Link
-                  className="group/link projectLink"
+                  className="group/link inline-flex items-baseline text-base font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300"
                   href="https://lefteris-l.github.io/layout_miami/"
                   target="_blank"
                   rel="noreferrer"
