06eb3846b41bd685050a115e87be06f267d49147
l_loizides <lefteris.loizides@mindgeek.com>
2024-04-21 19:04:24 +0300

    add script tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import jsonLd from 'jsonLd';
 import { type AppType } from "next/dist/shared/lib/utils";
-import Script from 'next/script';
-
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <>
     <Component {...pageProps} />
-    <Script id='jsonld' dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}/>
+    <script key='jsonld' dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}/>
   </>
 }
 
