435f02679924087c6494a100ce9758d46ed0944e
l_loizides <lefteris.loizides@mindgeek.com>
2024-04-27 19:47:38 +0300

    try diff markup for schema

--- jsonLd.ts
+++ jsonLd.ts
@@ -1,16 +1,29 @@
-import { type WithContext, type WebSite } from 'schema-dts'
-
-const jsonLd:WithContext<WebSite> = {
-  '@context': 'https://schema.org',
-  '@type': 'WebSite',
-  name: 'Lefteris Loizides',
-  url: 'https://lefterisloizides.com/',
-  alternateName: 'Lefteris\' Portfolio',
-  sameAs: [
-    'https://github.com/lefteris-l',
-    'https://instagram.com/lefteris.lz',
-    'https://www.linkedin.com/in/lefterisl/'
-  ],
-}
+const jsonLd = [
+  {
+    "@type": [
+      "http://schema.org/WebSite"
+    ],
+    "http://schema.org/alternateName": [
+      {
+        "@value": "Lefteris' Portfolio"
+      }
+    ],
+    "http://schema.org/author": [
+      {
+        "@value": "Lefteris Loizides"
+      }
+    ],
+    "http://schema.org/name": [
+      {
+        "@value": "Lefteris Loizides"
+      }
+    ],
+    "http://schema.org/url": [
+      {
+        "@id": "https://lefterisloizides.com/"
+      }
+    ]
+  }
+]
 
 export default jsonLd
