234dc2f02dfbf11733a5ceae46cc3ceb618b534b
Lefteris Loizides <lefteris.loizides@aylo.com>
2024-07-02 02:33:31 +0300

    update about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,11 +25,10 @@ const About = () => (
       <p className="mb-4">
         My main focus these days is working
         on projects at{" "}
-        <CustomLink href="http://aylo.com">Aylo</CustomLink> and building SaaS web apps. I&apos;ve also built a shopify store that is no longer active and a telegram bot.
+        <CustomLink href="http://aylo.com">Aylo</CustomLink> and studying to become a bug bounty hunter. I also work on projects for friends and family on occasion and build automation tools and scripts for anything that can be automated, using Bash.
       </p>
       <p>
-        When I&apos;m not coding, I&apos;m usually cooking up something delish, building my stock
-        portfolio, perfecting my techniques in MMA, or cruising down
+        When I&apos;m not coding, I&apos;m usually cooking up something delish, spending time with family and friends, perfecting my techniques in MMA, or cruising down
         the street in my Golf Mk4.
       </p>
     </div>
